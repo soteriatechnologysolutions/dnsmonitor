6bc24919ff305fed9c46a0d9f235383c45d39ada
Calliope Bailey <callie.bailey@heysoteria.com>
2023-10-17 14:01:56 -0500

    hello dns

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Rust development environment for dns monitor tool";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs, ...}: let 
+    system = "x86_64-linux";
+  in { 
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+        rust-analyzer
+      ];
+      shellHook = ''
+        cargo --version
+      '';
+    };
+  };
+}
