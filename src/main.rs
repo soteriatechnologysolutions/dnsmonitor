8ecdc9c021e4a19a09f4b5b0f1610f29406c7bd5
Calliope Bailey <callie.bailey@heysoteria.com>
2023-10-25 14:59:14 -0500

    Added a few basic API methods

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use axum::{
+    extract::Path,
     Router,
     http::StatusCode,
     routing::get,
@@ -12,7 +13,10 @@ use maud::{ html, Markup, };
 #[tokio::main]
 async fn main() {
     let site = Router::new()
-        .route("/", get(front_page));
+        .route("/", get(front_page))
+    //these routes mainly for debug (for now)
+        .route("/create/:domain", get(nc_create))
+        .route("/gethosts/:name/:tld", get(nc_get_hosts));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(site.into_make_service())
@@ -22,20 +26,71 @@ async fn main() {
 
 async fn front_page() -> (StatusCode, Markup) {
 
-    ( StatusCode::OK, draw_index(&querydns().await))
+    ( StatusCode::OK, draw_index(&query_dns().await) )
 }
 
 fn draw_index(contents: &str) -> Markup {
     html!((contents))
 }
 
-async fn querydns() -> String {
+//👽
+//👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽
+
+//CODE SO SLOPPY
+//TODO: REFACTOR 💋
+async fn nc_create(
+    Path(domain): Path<String>
+) -> (StatusCode, Markup) {
     let api_user = env::var("APIUSER").unwrap();
     let api_key = env::var("APIKEY").unwrap();
     let user_name = env::var("USERNAME").unwrap();
     let client_ip = env::var("CLIENTIP").unwrap();
 
-    let call = format!("https://api.sandbox.namecheap.com/xml.response?ApiUser={api_user}&ApiKey={api_key}&UserName={user_name}&ClientIp={client_ip}&Command=namecheap.domains.check&DomainList=heysoteria.com");
+    //TODO: This is not correct, need to use the format specified at
+    //https://www.namecheap.com/support/api/methods/domains/create/
+    //there has got to be a better way to do this 💔
+    let call = format!("https://api.sandbox.namecheap.com/xml.response?ApiUser={api_user}&ApiKey={api_key}&UserName={user_name}&ClientIp={client_ip}&Command=namecheap.domains.create&DomainName={domain}&Years=1&RegistrantFirstName=Hey&RegistrantLastName=Soteria&RegistrantAddress1=1815%20Central&RegistrantCity=Wichita&RegistrantStateProvince=Kansas&RegistrantPostalCode=67214&RegistrantCountry=US&RegistrantPhone=+3164487944&RegistrantEmailAddress=netadmin@heysoteria.com&TechFirstName=Hey&TechLastName=Soteria&TechAddress1=1815%20Central&TechCity=Wichita&TechStateProvince=Kansas&TechPostalCode=67214&TechCountry=US&TechPhone=+3164487944&TechEmailAddress=netadmin@heysoteria.com&AdminFirstName=Hey&AdminLastName=Soteria&AdminAddress1=1815%20Central&AdminCity=Wichita&AdminStateProvince=Kansas&AdminPostalCode=67214&AdminCountry=US&AdminPhone=+3164487944&AdminEmailAddress=netadmin@heysoteria.com&AuxBillingFirstName=Hey&AuxBillingLastName=Soteria&AuxBillingAddress1=1815%20Central&AuxBillingCity=Wichita&AuxBillingProvince=Kansas&AuxBillingPostalCode=67214&AuxBillingCountry=US&AuxBillingPhone=+3164487944&AuxBillingEmailAddress=netadmin@heysoteria.com");
+
+    let resp = reqwest::get(&call)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    ( StatusCode::OK, draw_index(&resp) )
+}
+
+//TODO: Need to be able to pass the domain we want to act on
+//to this function 
+//forget how to do this lol 
+async fn nc_get_hosts(
+        Path((name, tld)): Path<(String, String)>,
+) -> (StatusCode, Markup) {
+    let api_user = env::var("APIUSER").unwrap();
+    let api_key = env::var("APIKEY").unwrap();
+    let user_name = env::var("USERNAME").unwrap();
+    let client_ip = env::var("CLIENTIP").unwrap();
+
+    let call = format!("https://api.sandbox.namecheap.com/xml.response?ApiUser={api_user}&ApiKey={api_key}&UserName={user_name}&ClientIp={client_ip}&Command=namecheap.domains.dns.getHosts&SLD={name}&TLD={tld}");
+
+    let resp = reqwest::get(&call)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    ( StatusCode::OK, draw_index(&resp) )
+}
+
+async fn query_dns() -> String {
+    let api_user = env::var("APIUSER").unwrap();
+    let api_key = env::var("APIKEY").unwrap();
+    let user_name = env::var("USERNAME").unwrap();
+    let client_ip = env::var("CLIENTIP").unwrap();
+
+    let call = format!("https://api.sandbox.namecheap.com/xml.response?ApiUser={api_user}&ApiKey={api_key}&UserName={user_name}&ClientIp={client_ip}&Command=namecheap.domains.getList");
 
     let resp = reqwest::get(&call)
         .await
