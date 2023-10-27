46e35658d04d58690291a3024bf3b3a86e4b470f
Calliope Bailey <callie.bailey@heysoteria.com>
2023-10-27 12:14:22 -0500

    Got some very simple xml parsing for the front page, prints list of registered domains

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 
 use axum::{
     extract::Path,
@@ -6,9 +10,18 @@ use axum::{
     http::StatusCode,
     routing::get,
 };
-    
 
-use maud::{ html, Markup, };
+use maud::{ html, Markup, PreEscaped, DOCTYPE };
+
+//TODO: CAA records, SRV records
+//I think I'll need to do NS records too
+enum DnsRecord {
+    A { host: String, value: Ipv4Addr },
+    AAAA { host: String, value: Ipv6Addr },
+    MX { host: String, priority: u32, value: String },
+    CNAME { host: String, value: String },
+    TXT { host: String, value: String },
+}
 
 #[tokio::main]
 async fn main() {
@@ -30,7 +43,21 @@ async fn front_page() -> (StatusCode, Markup) {
 }
 
 fn draw_index(contents: &str) -> Markup {
-    html!((contents))
+    html! { 
+        (DOCTYPE)
+        html {
+            head {
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                title { "DNS Monitor Tool" } 
+            }
+            body {
+                pre {
+                    (PreEscaped(contents))
+                }
+            }
+        }
+    }
 }
 
 //👽
@@ -96,5 +123,64 @@ async fn query_dns() -> String {
         .await
         .unwrap();
 
-    resp
+    // let resp = env::var("DEBUG_RESPONSE").unwrap();
+
+    let mut reader = Reader::from_str(&resp);
+    let mut buf = Vec::new();
+    let mut skip_buf = Vec::new();
+    let mut domain_list = String::new();
+
+    //TODO: Using if let will make this much more readable
+    //I hate xml
+    //TODO: I think the recursion might not actually be unnecessary?
+    //look into it later
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e)) => {
+                match e.name().as_ref() {
+                    b"DomainGetListResult" => {
+                        loop {
+                            skip_buf.clear();
+                            match reader.read_event_into(&mut skip_buf) {
+                                //empty is for tags formatted like < attribute="xyz" />
+                                Ok(Event::Empty(f)) => {
+                                    match f.name().as_ref() {
+                                        b"Domain" => {
+                                            for i in f.attributes()
+                                            {
+                                                let att = i.unwrap();
+                                                if att.key.local_name().as_ref() == b"Name"
+                                                {
+                                                    let domain = att.unescape_value().unwrap();
+                                                    println!("{}", domain);
+                                                    domain_list.push_str(domain.as_ref());
+                                                    domain_list.push_str("\n");
+                                                }
+                                            }
+                                        },
+                                        _ => (),
+                                    }
+                                },
+                                Ok(Event::End(e)) => {
+                                    if e.name().as_ref() == b"DomainGetListResult" {
+                                        break;
+                                    }
+                                }
+                                Err(e) => panic!("Error at position {}: {:?}",
+                                                  reader.buffer_position(), e),
+                                _ => (),
+                            }
+                        }
+                    },
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
+    }
+
+    buf.clear();
+    domain_list
 }
